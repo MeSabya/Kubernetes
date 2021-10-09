7ea5df1bc8fd086425d7b229b357fe80b00d7bce
Sabyasachi Nayak <nayak.saabyasachi@gmail.com>
2021-10-09 12:25:06 +0530

    Counter-app first deployment

---
+++ counter-app/script.sh
@@ -0,0 +1,11 @@
+cd app/
+touch counter.txt
+declare -i var=$(cat counter.txt)
+for (( ;; ))
+do
+    cat counter.txt
+    var=$((var + 1))
+    echo $var >counter.txt
+    sleepSeconds=$(echo $((1 + $RANDOM % 3)))
+    sleep $sleepSeconds
+done
